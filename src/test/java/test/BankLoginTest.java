3b768d1a03ea8bdfd8bac9cb649a56f9e02d4485
Andrey Turchak <andrewturchak78@mail.ru>
2023-05-02 15:57:54 +0300

    code and test added

---
+++ src/test/java/test/BankLoginTest.java
@@ -0,0 +1,31 @@
+package test;
+
+import data.DataHelper;
+import data.SQLHelper;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import page.LoginPage;
+
+
+import static com.codeborne.selenide.Selenide.open;
+import static data.SQLHelper.cleanDatabase;
+
+
+public class BankLoginTest {
+
+    @AfterAll
+    static void teardown() { cleanDatabase(); }
+
+    @Test
+    @DisplayName("Should successfully login to dashboard with exist login and password from sut test data")
+    void shouldSuccessfulLogin() {
+        var loginPage = open("http://localhost:9999", LoginPage.class);
+        var authInfo = DataHelper.getAuthInfoWithTestData();
+        var verificationPage = loginPage.validLogin(authInfo);
+        verificationPage.verifyVerificationPageVisibility();
+        var verificationCode = SQLHelper.getVerificationCode();
+        verificationPage.validVerify(verificationCode.getCode());
+    }
+
+}
